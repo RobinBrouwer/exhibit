4637ccb114289c8b0e9dd26847d4744359f01a79
Phil Nash <philnash@gmail.com>
2012-07-17 18:30:22 +0100

    Nest test unit tests at test/unit/presenters

--- lib/generators/shoulda/presenter_generator.rb
+++ lib/generators/shoulda/presenter_generator.rb
@@ -3,7 +3,7 @@ module Shoulda
     source_root File.expand_path('../templates', __FILE__)
 
     def create_test_file
-      template 'presenter_test.rb', File.join('test/presenters', "#{singular_name}_presenter_test.rb")
+      template 'presenter_test.rb', File.join('test/unit/presenters', "#{singular_name}_presenter_test.rb")
     end
   end
 end
