474434ce339537cf81d991e38da0772f420b42a8
Adam <electronicbattleweapon7@gmail.com>
2012-07-18 15:45:07 +0100

    Fix presenter class name

--- lib/generators/shoulda/templates/presenter_test.rb
+++ lib/generators/shoulda/templates/presenter_test.rb
@@ -4,7 +4,7 @@ class <%= class_name %>PresenterTest < ActionView::TestCase
   context "a presenter for <%= class_name %>" do
     setup do
       @<%= singular_name %> = <%= class_name %>.new
-      @<%= singular_name %>_presenter = <%= class_name %>_presenter.new(@<%= singular_name %>, view)
+      @<%= singular_name %>_presenter = <%= class_name %>Presenter.new(@<%= singular_name %>, view)
     end
 
     # Replace this with your actual tests
