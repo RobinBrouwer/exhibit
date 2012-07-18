3925cf28e6536cb7358eb5d57a71d81fa27f6ba8
Adam <electronicbattleweapon7@gmail.com>
2012-07-18 15:32:03 +0100

    add setup for Test::Unit, Shoulda and RSpec tests

--- lib/generators/shoulda/templates/presenter_test.rb
+++ lib/generators/shoulda/templates/presenter_test.rb
@@ -1,4 +1,15 @@
 require 'test_helper'
 
 class <%= class_name %>PresenterTest < ActionView::TestCase
+  context "a presenter for <%= class_name %>" do
+    setup do
+      @<%= singular_name %> = <%= class_name %>.new
+      @<%= singular_name %>_presenter = <%= class_name %>_presenter.new(@<%= singular_name %>)
+    end
+
+    # Replace this with your actual tests
+    should "do something" do
+      assert true
+    end
+  end
 end
